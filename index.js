b5d795232ebb45b31adbc7d228caa4dc73464d05
Gopal M <46003265+gopalm-ai@users.noreply.github.com>
2020-04-28 14:44:35 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,126 +1,65 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const request = require('request');
-const path = require('path');
-const Blockchain = require('./blockchain/');
-const PubSub = require('./app/pubsub');
-const TransactionPool = require('./wallet/transaction-pool');
-const Wallet = require('./wallet');
-const TransactionMiner = require('./app/transaction-miner');
+const Block = require('./block');
+const Transaction = require('../wallet/transaction');
+const Wallet = require('../wallet');
+const { cryptoHash } = require('../util');
+const { REWARD_INPUT, MINING_REWARD } = require('../config');
 
-const app = express();
-const blockchain = new Blockchain();
-const transactionPool = new TransactionPool();
-const wallet = new Wallet();
-const pubsub = new PubSub({ blockchain, transactionPool });
-const transactionMiner = new TransactionMiner({ blockchain, transactionPool, wallet, pubsub });
-
-const DEFAULT_PORT = 3000;
-const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
-
-app.use(bodyParser.json());
-
-app.get('/api/blocks', (req, res) => {
-    res.json(blockchain.chain);
-});
-
-app.get('/api/blocks/length', (req, res) => {
-    res.json(blockchain.chain.length);
-});
-
-app.get('/api/blocks/:id', (req, res) => {
-    const { id } = req.params;
-    const { length } = blockchain.chain;
-
-    const blocksReversed = blockchain.chain.slice().reverse();
-
-    let startIndex = (id-1) * 5;
-    let endIndex = id * 5;
-
-    startIndex = startIndex < length ? startIndex : length;
-    endIndex = endIndex < length ? endIndex : length;
-
-    res.json(blocksReversed.slice(startIndex, endIndex));
-});
-
-app.post('/api/mine', (req, res) => {
-    const { data } = req.body;
-
-    blockchain.addBlock({ data });
-
-    pubsub.broadcastChain();
-
-    res.redirect('/api/blocks');
-});
-
-app.post('/api/transact', (req, res) => {
-    const { amount, recipient } = req.body;
-
-    let transaction = transactionPool
-        .existingTransaction({ inputAddress: wallet.publicKey });
-
-    try {
-        if (transaction) {
-            transaction.update({ senderWallet: wallet, recipient, amount });
-        } else {
-            transaction = wallet.createTransaction({ 
-                recipient, 
-                amount,
-                chain: blockchain.chain 
-            });
-        }
-    } catch(error) {
-        return res.status(400).json({ type: 'error', message: error.message });
+class Blockchain {
+    constructor() {
+        this.chain = [Block.genesis()];
     }
 
-    transactionPool.setTransaction(transaction);
+    addBlock({ data }) {
+        const newBlock = Block.mineBlock({
+            lastBlock: this.chain[this.chain.length-1],
+            data
+        });
 
-    pubsub.broadcastTransaction(transaction);
+        this.chain.push(newBlock);
+    }
 
-    res.json({ type: 'success', transaction });
-});
-
-app.get('/api/transaction-pool-map', (req, res) => {
-    res.json(transactionPool.transactionMap);
-});
-
-app.get('/api/mine-transactions', (req, res) => {
-    transactionMiner.mineTransactions();
-
-    res.redirect('/api/blocks');
-});
-
-const syncWithRootState = () => {
-    request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-            const rootChain = JSON.parse(body);
-
-            console.log('replace chain on a sync with', rootChain);
-            blockchain.replaceChain(rootChain);
+    replaceChain(chain, validateTransactions, onSuccess) {
+        if (chain.length <= this.chain.length) {
+            console.error('The incoming chain must be longer');
+            return;
         }
-    });
-
-    request({ url: `${ROOT_NODE_ADDRESS}/api/transaction-pool-map` }, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-            const rootTransactionPoolMap = JSON.parse(body);
-
-            console.log('replace transaction pool map on a sync with', rootTransactionPoolMap);
-            transactionPool.setMap(rootTransactionPoolMap);
+        
+        if (!Blockchain.isValidChain(chain)) {
+            console.error('The incoming chain must be valid');
+            return;
         }
-    });
-};
 
-let PEER_PORT;
+        if (validateTransactions && !this.validTransactionData({ chain })) {
+            console.error('The incoming chain has invalid data');
+            return;
+        }
+        
+        if (onSuccess) onSuccess();
+        console.log('replacing chain with', chain);
+        this.chain = chain;
+    }
 
-if (process.env.GENERATE_PEER_PORT === 'true') {
-    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+    static isValidChain(chain) {
+        if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+            return false
+        };
+
+        for (let i=1; i<chain.length; i++) {
+            const { timestamp, lastHash, hash, nonce, difficulty, data } = chain[i];
+            const actualLastHash = chain[i-1].hash;
+            const lastDifficulty = chain[i-1].difficulty;
+
+            if (lastHash != actualLastHash) return false;
+
+            const validatedHash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
+
+            if (hash !== validatedHash) return false;
+
+            if (Math.abs(lastDifficulty - difficulty) > 1) return false;
+        }
+
+        return true;
+    }
 }
 
-const PORT = PEER_PORT || DEFAULT_PORT;
-app.listen(PORT, () => {
-    console.log(`listening of localhost:${PORT}`);
-
-    if (PORT != DEFAULT_PORT) {
-        syncWithRootState();
-    }
-});
+module.exports = Blockchain;
